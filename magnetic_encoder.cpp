2fdd0ee42cf3c24ed99a4f308e1429814ac78f95
AlexanderF011 <alexander.fried@colostate.edu>
2025-04-05 16:03:45 -0600

    Adds a program file
    The program file contains the basic functions for reading from the encoder. Created with platformio, but the necessary platfromio.ini file is only the default one. It will need to be expanded upon in the future to account for edge cases and have a different sampling method.

---
+++ magnetic_encoder.cpp
@@ -0,0 +1,99 @@
+#include <Arduino.h>
+
+/*  Magnetic Encoder Code for the AEAT-6600-T16 (C++ Version)
+*   Written by: Alexander Fried
+*
+*   Intended to be used with the Arduino UNO R3. Most of the code is derived from Dan Royer's(GitHub: i-make-robots)
+*   "aeat6600-t16-sensor-test" repository. The sensor should be setup to match the schematic from an unnamed user's
+*   (GitHub: 4answer) "AEAT-6600-T16" repository. 
+*   
+*   Created for data collection with a surveyor's wheel. You must record your specific wheel's radius under 
+*   "WHEEL_RAD" in the variable defintions section below.
+*   
+*   This code was written using the following sources as references:
+*   https://github.com/4nswer/AEAT-6600-T16 
+*   https://github.com/MarginallyClever/aeat6600-t16-sensor-test 
+*   https://forum.arduino.cc/t/absolute-rotary-encoder-ssi-spi-how/153103 
+*/
+
+// Pin Definitions:
+#define MAG_HI      3
+#define MAG_LO      2
+#define ALIGN_PIN   4
+#define PWM_PIN     7
+#define PWR_DWN     5
+#define PROG_PIN    9
+#define NCS_PIN     10
+#define CLK_PIN     11
+#define DATA_PIN    12
+
+// Variable Definitions:
+#define ANGLE_BITS      (10)
+#define BIT_COUNT       (10)
+#define ANGLE_PER_BIT   (360.0/(float)((long)1<<ANGLE_BITS))
+#define WHEEL_RAD       (0.25)               // Radius of wheel in meters 
+#define PI              3.1415926
+#define DIST_PER_ANGLE  ((WHEEL_RAD * 2 * PI)/360.0)
+#define DIST_PER_BIT    ((DIST_PER_ANGLE)/(ANGLE_PER_BIT))
+double totalDistance = 0;
+double pastPosition = 0;
+
+void setup() {
+  pinMode(MAG_HI, INPUT);                   // Outputs HIGH when magnetic field is too strong
+  pinMode(MAG_LO, INPUT);                   // Outputs HIGH when magnetic field is too weak
+  pinMode(ALIGN_PIN,OUTPUT);                // Set LOW for normal operation, set HIGH for alignment mode
+  pinMode(PWR_DWN,OUTPUT);                  // Set HIGH to begin powering-off the device 
+  pinMode(PROG_PIN,OUTPUT);                 // Set LOW for normal operation, set HIGH for OTP programming (pull-down)
+  pinMode(NCS_PIN,OUTPUT);                  // Set HIGH for internal pull-up
+  pinMode(DATA_PIN, INPUT);                 // Outputs sensor readings
+  pinMode(CLK_PIN, OUTPUT);                 // Set HIGH to begin the clock
+  //digitalWrite(NCS_PIN, HIGH);
+  digitalWrite(CLK_PIN, HIGH);
+  Serial.begin(57600);                      // Sets the data rate to the specified bits-per-second
+  Serial.println("Surveyor's Wheel Distance Calculations: ");
+}
+
+void loop() {
+  //digitalWrite(NCS_PIN, LOW);
+  delay(3000);
+  double distTraveled = getDistTraveled();
+  totalDistance += distTraveled;
+  Serial.print("Current Distance from Start: ");
+  Serial.print(totalDistance);
+  Serial.println("m");
+  Serial.print("Distance since last Reading: ");
+  Serial.print(distTraveled);
+  Serial.print("m");
+}
+
+// Function Definitions:
+double getDistTraveled() {
+  double currPosition = (readPosition() * DIST_PER_BIT);
+  double distTraveled = currPosition - pastPosition;
+  pastPosition = currPosition;
+  return distTraveled;
+}
+
+uint32_t readPosition() {
+  uint32_t data = 0;
+  uint8_t instream = 0;
+
+  digitalWrite(NCS_PIN, LOW);
+  delayMicroseconds(1);
+  digitalWrite(CLK_PIN, LOW);
+  delayMicroseconds(1);
+
+  for(int i = 0; i < BIT_COUNT; i++) {
+    digitalWrite(CLK_PIN, HIGH);
+    delayMicroseconds(1);
+    digitalWrite(CLK_PIN, LOW);
+    delayMicroseconds(1);
+    instream = digitalRead(DATA_PIN);
+    data = (data << 1) | instream;
+  }
+
+  digitalWrite(NCS_PIN, HIGH);
+  digitalWrite(CLK_PIN, HIGH);
+
+  return data;
+}
